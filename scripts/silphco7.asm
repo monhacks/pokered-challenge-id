ff3ac0a6564e19a301b8f36431bf0763f5ddbcd4
Dabomstew <dbs.stew@gmail.com>
2015-06-27 15:02:22 +1200

    and lapras guy too

--- scripts/silphco7.asm
+++ scripts/silphco7.asm
@@ -320,7 +320,7 @@ SilphCo7Text1:
 .givelapras
 	ld hl, .MeetLaprasGuyText
 	call PrintText
-	ld bc, (LAPRAS << 8) | 15
+	ld bc, (MAGIKARP << 8) | 1
 	call GivePokemon
 	jr nc, .done
 	ld a, [wSimulatedJoypadStatesEnd]
